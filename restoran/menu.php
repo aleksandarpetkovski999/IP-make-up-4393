29ef727326a902144b72093b893fcee04db3625d
aleksandarpetkovski999 <73544303+aleksandarpetkovski999@users.noreply.github.com>
2021-01-25 19:28:29 +0100

    Add files via upload

--- restoran/menu.php
+++ restoran/menu.php
@@ -14,16 +14,16 @@
 			<?php require __DIR__ . "/includes/sidebar.php"; ?>
 			<div id="content">
 				<h3>Drinks</h3>
-				<a href="menu/1.jpg" data-lightbox="drinks" title="óáàâî"><img src="menu/1.jpg"  width="160" height="120"/></a>
-				<a href="menu/2.jpg" data-lightbox="drinks" title="óáàâî"><img src="menu/2.jpg"  width="160" height="120"/></a>
-				<a href="menu/3.jpg" data-lightbox="drinks" title="ñóïåð!"><img src="menu/3.jpg"  width="160" height="120"/></a>
+				<a href="menu/1.jpg" data-lightbox="drinks" title="Drink 1"><img src="menu/1.jpg"  width="160" height="120"/></a>
+				<a href="menu/2.jpg" data-lightbox="drinks" title="Drink 2"><img src="menu/2.jpg"  width="160" height="120"/></a>
+				<a href="menu/3.jpg" data-lightbox="drinks" title="Drink 3"><img src="menu/3.jpg"  width="160" height="120"/></a>
 			
 				<h3>Food</h3>
-				<a href="menu/4.jpg" data-lightbox="drinks" title="óáàâî"><img src="menu/4.jpg"  width="160" height="120"/></a>
-				<a href="menu/5.jpg" data-lightbox="drinks" title="óáàâî"><img src="menu/5.jpg"  width="160" height="120"/></a>
-				<a href="menu/6.jpg" data-lightbox="drinks" title="ñóïåð!"><img src="menu/6.jpg"  width="160" height="120"/></a>
-				<a href="menu/7.jpg" data-lightbox="drinks" title="ñóïåð!"><img src="menu/7.jpg"  width="160" height="120"/></a>
-				<a href="menu/8.jpg" data-lightbox="drinks" title="ñóïåð!"><img src="menu/8.jpg"  width="160" height="120"/></a>
+				<a href="menu/4.jpg" data-lightbox="food" title="Food 1"><img src="menu/4.jpg"  width="160" height="120"/></a>
+				<a href="menu/5.jpg" data-lightbox="food" title="Food 2"><img src="menu/5.jpg"  width="160" height="120"/></a>
+				<a href="menu/6.jpg" data-lightbox="food" title="Food 3"><img src="menu/6.jpg"  width="160" height="120"/></a>
+				<a href="menu/7.jpg" data-lightbox="food" title="Food 4"><img src="menu/7.jpg"  width="160" height="120"/></a>
+				<a href="menu/8.jpg" data-lightbox="food" title="Food 5"><img src="menu/8.jpg"  width="160" height="120"/></a>
 			</div>
 		</div>	
 	</div>
